370261f2e0c9e1faec3e697c540147d196a288e9
brett060102 <esampson@suse.com>
2023-02-22 14:48:27 +0000

    Update wrappers to support colon in file path, needed for osc

--- dev-env-wrapper.sh
+++ dev-env-wrapper.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 PATH=/usr/bin:/bin
 
-podman run  --security-opt label=disable -it -v ${PWD}:/work -v ${HOME}:${HOME} --userns=keep-id --rm dev-env "$(basename "${0}")" $@
+KEEP_USERID=""
+if [[ $(id -ru) != "0" ]]; then
+    KEEP_USERID="--userns=keep-id"
+fi
 
+LINK_DIR=$(mktemp -d -p /tmp)
+ln -s $(pwd)  ${LINK_DIR}/work
+ln -s ${HOME} ${LINK_DIR}/home
+
+podman run  --security-opt label=disable -it -v ${LINK_DIR}/work:/work -v ${LINK_DIR}/home:${HOME} ${KEEP_USERID} --rm dev-env "$(basename "${0}")" "$@"
+
+rm ${LINK_DIR}/work ${LINK_DIR}/home
+rmdir ${LINK_DIR}
